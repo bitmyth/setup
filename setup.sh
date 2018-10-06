c111bf770e3d13e7567e004355817e79954ba96c
bitmyth <bitmyth@outlook.com>
2018-10-06 23:23:46 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,16 @@
 #.vimrc
-cp .vimrc ~/.vimrc
-echo 'HISTTIMEFORMAT="%F %T `whoami`"  ' >>/etc/bashrc
+cat >> ~/.vimrc <<EOF
+set nu
+syntax on
+set sw=4
+set ts=4
+set autoindent
+set cindent
+set incsearch
+EOF
+
+# set default editor to vim
+echo 'export EDITOR=vim' >>/etc/profile
+
+# HISTTIMEFORMAT 设置历史命令时间的格式
+echo 'HISTTIMEFORMAT="%F %T `whoami`"  ' >>/etc/profile
